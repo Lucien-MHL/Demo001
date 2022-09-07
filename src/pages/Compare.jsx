6e15d2f6c94eae50efe655aab6f1f841225b82c2
Lucien <lucienenginnerlife@gmail.com>
2022-09-07 09:12:49 +0800

    feat isRender variable to decide Search component render or not

--- src/pages/Compare.jsx
+++ src/pages/Compare.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Footer from '../components/Footer.jsx'
 import Header from '../components/Header.jsx'
 import styles from '../sass/main.module.scss'
 
 function Compare() {
+  const render = useRef(false)
   return (
     <div className={styles.container}>
-      <Header />
+      <Header isRender={render} />
 
       <div className={styles.shrink_hidden}> </div>
       <Footer />
